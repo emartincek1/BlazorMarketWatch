f679f1e4112ebcfae74c63372cf6b74d1d98667e
emartincek1 <evan.martincek@gmail.com>
2024-03-25 17:03:06 -0500

    create stock search razor component

---
+++ BlazorMarketWatch/BlazorMarketWatch.Web/Components/Pages/SearchStocksBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorMarketWatch.Web.Components.Pages
+{
+    public class SearchStocksBase:ComponentBase
+    {
+        public List<string> TopStocks { get; set; }
+        public string TextValue { get; set; }
+
+        protected override void OnInitialized()
+        {
+            TopStocks = new List<string> { "AAPL", "XOM", "WMT", "TSLA", "PYPL", "NFLX", "MSFT", "MMM", "META", "KO", "GOOGL", "COST" };
+        }
+    }
+}
