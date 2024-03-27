1e8ce9ee96cb1d5ef2e1232ade75d96d616acb85
emartincek1 <evan.martincek@gmail.com>
2024-03-27 13:32:30 -0500

    add authentication

--- BlazorMarketWatch/BlazorMarketWatch.Web/Program.cs
+++ BlazorMarketWatch/BlazorMarketWatch.Web/Program.cs
@@ -1,7 +1,12 @@
 using BlazorMarketWatch.Web.Components;
 using BlazorMarketWatch.Web.Services;
 using BlazorMarketWatch.Web.Services.Contracts;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using MudBlazor.Services;
+using BlazorMarketWatch.Web.Components.Account;
+using BlazorMarketWatch.Web.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,10 +19,42 @@ builder.Services.AddMudServices();
 builder.Services.AddHttpClient();
 builder.Services.AddTransient<IStockService, StockService>();
 
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+})
+    .AddIdentityCookies();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -31,5 +68,8 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+
+app.MapAdditionalIdentityEndpoints();
 
 app.Run();
