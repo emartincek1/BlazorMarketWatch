e5f617a04fbe1c26f48809a2a5edb67b0826cd7b
emartincek1 <evan.martincek@gmail.com>
2024-03-28 11:00:19 -0500

    create user stock repository

---
+++ BlazorMarketWatch/BlazorMarketWatch.Web/Repositories/Contracts/IUserStockRepository.cs
@@ -0,0 +1,11 @@
+﻿using BlazorMarketWatch.Web.Entities;
+
+namespace BlazorMarketWatch.Web.Repositories.Contracts
+{
+    public interface IUserStockRepository
+    {
+        Task<IEnumerable<UserStock>> GetUserStocks(string userId);
+        Task<UserStock> AddUserStock(string ticker, string userId);
+        Task<UserStock> DeleteUserStock(int id);
+    }
+}
---
+++ BlazorMarketWatch/BlazorMarketWatch.Web/Repositories/UserStockRepository.cs
@@ -0,0 +1,57 @@
+﻿using BlazorMarketWatch.Web.Data;
+using BlazorMarketWatch.Web.Entities;
+using BlazorMarketWatch.Web.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorMarketWatch.Web.Repositories
+{
+    public class UserStockRepository : IUserStockRepository
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public UserStockRepository(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        private async Task<bool> UserStockExists(string ticker, string userId)
+        {
+            return await applicationDbContext.UserStocks.AnyAsync(s => s.Ticker == ticker &&
+                                                                s.UserId == userId);
+        }
+
+        public async Task<UserStock> AddUserStock(string ticker, string userId)
+        {
+            if (await UserStockExists(ticker, userId) == false)
+            {
+                var userStock = new UserStock
+                {
+                    Ticker = ticker,
+                    UserId = userId
+                };
+
+                if (userStock != null)
+                {
+                    var result = await applicationDbContext.UserStocks.AddAsync(userStock);
+                    await applicationDbContext.SaveChangesAsync();
+                    return result.Entity;
+                }
+            }
+
+            return null;
+        }
+
+        public Task<UserStock> DeleteUserStock(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<UserStock>> GetUserStocks(string userId)
+        {
+            var userStocks = await (from userStock in applicationDbContext.UserStocks
+                                    where userStock.UserId == userId
+                                    select userStock).ToListAsync();
+            return userStocks;
+        }
+    }
+}
